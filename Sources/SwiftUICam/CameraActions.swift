f23329ca9f73aab9295e2e93ab6c954809a2dad5
Pierre Véron <pierre.veron@outlook.fr>
2020-03-31 23:06:32 +0200

    Extend protocol to make it easier and faster to implement the actions

--- Sources/SwiftUICam/CameraActions.swift
+++ Sources/SwiftUICam/CameraActions.swift
@@ -9,8 +9,29 @@
 import Foundation
 
 public protocol CameraActions {
-    func takePhoto()
-    func toggleVideoRecording()
-    func rotateCamera()
-    func changeFlashMode()
+    func takePhoto(events: UserEvents)
+    func toggleVideoRecording(events: UserEvents)
+    func rotateCamera(events: UserEvents)
+    func changeFlashMode(events: UserEvents)
+}
+
+extension CameraActions {
+    func takePhoto(events: UserEvents) {
+        events.didAskToCapturePhoto = true
+    }
+    
+    func toggleVideoRecording(events: UserEvents) {
+        if events.didAskToRecordVideo {
+            events.didAskToStopRecording = true
+        } else {
+            events.didAskToRotateCamera = true
+        }
+    }
+    func rotateCamera(events: UserEvents) {
+        events.didAskToRotateCamera = true
+    }
+    
+    func changeFlashMode(events: UserEvents) {
+        events.didAskToChangeFlashMode
+    }
 }
